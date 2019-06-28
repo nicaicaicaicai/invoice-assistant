ab2f22184277aed68e6528830ab1b35d5ce84259
panwei <1184734081@qq.com>
2019-06-28 18:11:01 +0800

    first commit

---
+++ config/dev.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    NODE_ENV: '"development"'
+  },
+  defineConstants: {},
+  weapp: {},
+  h5: {
+    devServer: {
+      port: 9900,
+      open: false
+    }
+  }
+}
